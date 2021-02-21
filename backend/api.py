d437519c863a67e6064b009c4c91b9cd4d8114e9
Zachary Schaffer <zachary.schaffer12@gmail.com>
2021-02-21 12:38:28 -0500

    Add conversion functionality

--- backend/api.py
+++ backend/api.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, jsonify
-app = Flask(__name__)
+from flask import Flask, request, jsonify, json
+import requests
 
+app = Flask(__name__)
+valid_currencies = {'CAD', 'HKD', 'ISK', 'PHP', 'DKK', 'HUF', 'CZK', 'GBP', 'RON', 'SEK', 'IDR', 'INR', 'BRL', 'RUB', 'HRK', 'JPY', 'THB', 'CHF', 'EUR', 'MYR', 'BGN', 'TRY', 'CNY', 'NOK', 'NZD', 'ZAR', 'USD', 'MXN', 'SGD', 'AUD', 'ILS', 'KRW', 'PLN'}
 
 @app.route('/')
 def hello():
@@ -8,8 +10,28 @@ def hello():
 
 @app.route('/api/convert', methods=['POST'])
 def convert():
-    print(request.json)
-    return jsonify('Test')
+    body = json.loads(request.data)
 
+    try:
+        base_currency = str.upper(body['currency'])
+        amount = float(body['amount'])
+        desired_currency = str.upper(body['convertTo'])
+    
+    except KeyError:
+        return 'Malformed request body. Excpected currency, amount, and convertTo fields.', 400
+
+    # Check if base currency and desired currency is valid
+    if base_currency not in valid_currencies or desired_currency not in valid_currencies:
+        return 'Invalid base/target currency', 400
+
+    response = requests.get('https://api.exchangeratesapi.io/latest?base=' + base_currency)
+    conversions = response.json()['rates']
+    conversion_rate = float(conversions[desired_currency])
+    result = dict()
+    result['currency'] = desired_currency
+    result['amount'] = amount * conversion_rate
+
+    return jsonify(result)
+    
 if __name__ == '__main__':
     app.run()
